dceddb792543100af3622cdd049aed861784e37a
Mike <mike.klemarewski@gmail.com>
2016-10-31 09:26:45 -0700

    Add dragging to the life counter

--- app/components/LifeCounter/index.jsx
+++ app/components/LifeCounter/index.jsx
@@ -3,25 +3,64 @@ import {PropTypes} from 'react'
 import styles from './style.css'
 import Arrows from 'babel!react-svg!../../../icons/arrows.svg'
 
-const changeOnClick = function(onLifeChange) {
-    return (event) => {
-        let clickY = event.clientY
+const DRAG_THRESHOLD = 20 
+
+class LifeCounter extends React.Component {
+    constructor(props) {
+        super(props)
+        this.didDrag = false
+        this.touchY = 0
+        this.accumulator = 0
+
+        this.touchStart = this.touchStart.bind(this)
+        this.changeOnClick = this.changeOnClick.bind(this)
+        this.changeOnDrag = this.changeOnDrag.bind(this)
+    }
+    
+    changeOnClick (event) {
+        if (this.didDrag) {
+            this.didDrag = false
+            this.touchY = 0
+            this.accumulator = 0
+            return
+        }
+
+        let clickY = event.changedTouches[0].clientY
         let targetRect = event.target.getBoundingClientRect()
 
         let targetHeight = targetRect.bottom - targetRect.top
         let midPoint = targetRect.top + (targetHeight / 2)
 
-        clickY < midPoint ? onLifeChange(1) : onLifeChange(-1)
+        clickY < midPoint ? this.props.onLifeChange(1) : this.props.onLifeChange(-1)
     }
-}
 
-const LifeCounter = ({lifeTotal, onLifeChange}) => {
-    return (
-        <div className={styles.lifeCounter} onClick={changeOnClick(onLifeChange)}>
-            {lifeTotal}
-            <Arrows className={styles.arrows} />
-        </div>
-    )
+    touchStart (event) {
+        this.touchY = event.changedTouches[0].clientY 
+    }
+
+    changeOnDrag (event) {
+        this.didDrag = true
+        this.accumulator += this.touchY - event.changedTouches[0].clientY
+        this.touchY = event.changedTouches[0].clientY
+
+        this.props.onLifeChange(~~(this.accumulator / DRAG_THRESHOLD))
+        this.accumulator %= DRAG_THRESHOLD
+    }
+
+    render() {
+        let {lifeTotal, onLifeChange} = this.props
+        return (
+            <div
+                className={styles.lifeCounter}
+                onTouchStart={this.touchStart}
+                onTouchEnd={this.changeOnClick}
+                onTouchMove={this.changeOnDrag}
+            >
+                {lifeTotal}
+                <Arrows className={styles.arrows} />
+            </div>
+        )
+    }
 }
 
 LifeCounter.defaultProps = {
