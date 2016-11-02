24022eb6b3431ea0705fd57dbfb021ff26ba56e1
Mike <mike.klemarewski@gmail.com>
2016-11-02 12:08:30 -0700

    Start on new game screen

--- app/containers/new-game/index.jsx
+++ app/containers/new-game/index.jsx
@@ -1,5 +1,7 @@
 import {connect} from 'react-redux'
 
+import Stepper from '../../components/stepper'
+
 import styles from './style.css'
 
 class NewGame extends React.Component {
@@ -7,7 +9,19 @@ class NewGame extends React.Component {
     render() {
         return (
             <div className={styles.wrapper}>
-                <h2>New Game</h2>
+                <div className={styles.row}>
+                    <h1>New Game</h1>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.label}>Players:</div>
+                    <div>
+                        <Stepper number={2} />
+                    </div>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.sublabel}>Commander Dammage</div>
+                    <input type="checkbox"/>
+                </div>
             </div>
         )
     }
